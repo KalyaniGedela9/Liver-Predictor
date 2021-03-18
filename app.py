cc113908b761f42ecf97275aec21478a7a662e55
kalyani g <80562086+KalyaniGedela9@users.noreply.github.com>
2021-03-18 11:44:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def predict():
         return render_template('front.html')
 if __name__=="__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True,host='0.0.0.0'port=port)
+    app.run(host='0.0.0.0',port=port,debug=True)
